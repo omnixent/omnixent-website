5234a2d99eecbeca95d2722697794215cfcfa98a
thejoin95 <miki.lombi@gmail.com>
2021-05-05 15:39:52 +0200

    feat: init responsive in targethero and targetbubble

--- components/molecules/TargetBubble/index.js
+++ components/molecules/TargetBubble/index.js
@@ -3,8 +3,8 @@ import { Box, Flex, Text } from '@chakra-ui/layout';
 
 export default function TargetBubble({ target, description, icon }) {
   return (
-    <Flex borderRadius={35} backgroundColor="gray.200" w={500} p={5}>
-      <Box mr={5}>
+    <Flex direction={['column', 'row']} borderRadius={35} backgroundColor="gray.200" w={['100%', 200, 350, 480, 500]} p={5}>
+      <Box alignSelf={['center', 'auto']} mr={5}>
         <Flex
           backgroundColor="white"
           borderRadius="full"
@@ -15,7 +15,7 @@ export default function TargetBubble({ target, description, icon }) {
           <Image src={`/featuresIcons/${icon}.svg`} w={75} h={75} />
         </Flex>
       </Box>
-      <Box>
+      <Box textAlign={['center', 'left']}>
         <Text color="gray.700" fontWeight="bold" mb={2}>
           {' '}
           {target}{' '}
