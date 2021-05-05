9cbb464192ea95448fb34ee663f25f73d1e4c509
thejoin95 <miki.lombi@gmail.com>
2021-05-05 15:21:46 +0200

    feat: add responsive on phones and tablet

--- components/organisms/HomepageHero/index.js
+++ components/organisms/HomepageHero/index.js
@@ -10,17 +10,17 @@ export default function HomepageHero() {
         background="linear-gradient(111.52deg, rgba(255, 189, 128, 0.182) 4.65%, rgba(255, 128, 136, 0.4628) 97.16%)"
         borderRadius="0px 72px 72px 0px"
         boxShadow="xl"
-        h={[400, 600]}
-        width={['95vw', '70vw']}
+        h={[380, 600]}
+        width={['95vw', '85vw', '70vw']}
         position="absolute"
         left={0}
         zIndex={-1}
       />
 
-      <Box zIndex={1} w={[480, 768, 992, 1280, 1400]} m="auto" pt={[10, 120]} px={[5, 0]}>
+      <Box zIndex={1} w={[480, 768, '100%', '100%', 1400]} m="auto" pt={[10, 30, 50, 50, 120]} px={[5]}>
         <Flex justifyContent="space-between">
-          <Box w={['85vw', '70vw']}>
-            <Heading color="purple.900" fontSize={["2xl", "6xl"]} lineHeight="short">
+          <Box w={['85vw', '75vw', '70vw']}>
+            <Heading color="purple.900" fontSize={["2xl", "3xl", "5xl", "6xl"]} lineHeight="short">
               Discover what people <br />
               search on the web.
             </Heading>
@@ -35,12 +35,12 @@ export default function HomepageHero() {
               <Button>Learn more</Button>
             </Box>
           </Box>
-          <Box display={['none', 'block']} mt={-150}>
+          <Box w="100%" alignSelf={['center', 'center', 'center', 'flex-end', 'flex-start']} display={['none', 'block']} mt={-150}>
             <Image
               src="/illustrations/rocket.png"
               layout="intrinsic"
-              width="750"
-              height="750"
+              width="700"
+              height="700"
               quality={95}
             />
           </Box>
